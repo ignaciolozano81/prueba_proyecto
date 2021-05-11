08c81d37974555fae04ca9f8bff02e5a19613635
ignaciolozano81 <ignaciolozano81@gmail.com>
2021-05-11 02:49:12 -0300

    branch nacho

--- pruebagit/main.cpp
+++ pruebagit/main.cpp
@@ -7,6 +7,7 @@ int main()
     cout << "Hello world" << endl;
     cout << "Hello world2\n" << endl;
     cout << "Hello world new branch2!\n" << endl;
+    cout << "Hello world branch nacho!\n" << endl;
 
     return 0;
 }
